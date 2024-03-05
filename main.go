387f64e1183a0af8aa938c9028b8d9dffd1176b5
lkorsman <lukekorsman@gmail.com>
2024-03-05 10:23:33 -0600

    Updated main.go with handling rpc calls

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
-package hockey_player_grpc
+package main
 
 import (
 	"context"
 	"github.com/lkorsman/hockey-player-grpc/player"
+	"google.golang.org/grpc"
 	"log"
 	"net"
 )
@@ -13,7 +14,16 @@ type myPlayerServer struct {
 
 func (s myPlayerServer) Create(ctx context.Context, req *player.PlayerRequest) (*player.PlayerResponse, error) {
 	log.Println("Received message from Create")
+	log.Printf("Player name: %s", req.Name)
+	log.Printf("Goals name: %s", req.Goal)
 	return &player.PlayerResponse{
+		Status: "ok",
+	}, nil
+}
+
+func (s myPlayerServer) AddGoal(ctx context.Context, req *player.Goal) (*player.GoalResponse, error) {
+	log.Println("Received message from AddGoal")
+	return &player.GoalResponse{
 		Status: "ok",
 	}, nil
 }
@@ -23,5 +33,14 @@ func main() {
 
 	if err != nil {
 		log.Fatalf("cannot create listener: %s", err)
+	}
+
+	serverRegistrar := grpc.NewServer()
+	service := &myPlayerServer{}
+	player.RegisterPlayerServer(serverRegistrar, service)
+	err = serverRegistrar.Serve(lis)
+
+	if err != nil {
+		log.Fatalf("impossible to serve: %s", err)
 	}
 }
